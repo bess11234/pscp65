c8ada51c2787c67043c5050f9ebfdfe292a00600
bess11234 <109953254+bess11234@users.noreply.github.com>
2022-11-05 13:16:26 +0700

    solved all

--- weeks 13/PedPonFire.py
+++ weeks 13/PedPonFire.py
@@ -1,18 +1,31 @@
 """PedPonFire"""
 def main():
     """PedPonFire"""
-    dic, lis, lis2 = {}, [], []
-    for i in range(int(input())):
-        dic.update({i : int(input())})
+    lis, num = [], int(input())
+    for i in range(num):
+        lis.append(int(input()))
     gas = int(input())
     count = 0
-    for i in dic:
-        for j in range(len(dic)):
-            tmp = 0
-            if i != j:
-                tmp += dic[i]+dic[j]
-            if tmp == gas:
-                pi
-                count += 1
+    lis = sorted(list(filter(lambda num: num <= gas, lis)))
+    set_lis = set(lis)
+    while True:
+        if set_lis == set():
+            break
+        for i in set_lis:
+            tmp = lis.count(i)
+            tmp2 = gas-i
+            if lis.count(tmp2) > 0:
+                if tmp2 == i:
+                    for j in range(tmp-1, 0, -1):
+                        count += j
+                    set_lis.remove(i)
+                else:
+                    count += lis.count(tmp2)*tmp
+                    set_lis.remove(tmp2)
+                    set_lis.remove(i)
+                break
+            else:
+                set_lis.remove(i)
+                break
     print(count)
 main()
